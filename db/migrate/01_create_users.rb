6811ed17ee0bba2d525efc2c46bd8ae3b4659137
Robert Laws <roblaws@me.com>
2017-10-27 17:49:11 +0000

    automatically backed up by learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,3 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
-  
+  def change
+    create_table :users do |t|
+      t.sting :name
+      t.string :email
+      t.string :password
+    end
+  end
 end
